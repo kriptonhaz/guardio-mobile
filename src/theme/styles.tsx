84f4706692a26fd50f4a138c1382b1614f83c029
kriptonhaz <kriptonhaz@gmail.com>
2024-06-16 23:21:23 +0700

    add style box transparent

--- src/theme/styles.tsx
+++ src/theme/styles.tsx
@@ -1,8 +1,19 @@
 import {StyleSheet} from 'react-native';
+import {ms, mvs} from 'react-native-size-matters';
+import Colors from './colors';
 
 const Styles = StyleSheet.create({
   backgroundScreen: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  boxTransparent: {
+    width: 'auto',
+    height: 'auto',
+    backgroundColor: Colors.Transparent[1],
+    borderRadius: mvs(20),
+    padding: ms(24),
     justifyContent: 'center',
     alignItems: 'center',
   },
