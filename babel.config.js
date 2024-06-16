58ccfd6e0ee076acf39018a93a897db31dece946
kriptonhaz <kriptonhaz@gmail.com>
2024-06-16 23:01:36 +0700

    setup alias import

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    // ... other configs, if any
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@route': './src/route',
+          '@screen': './src/screen',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+
+    // ... other configs, if any
+  ],
 };
