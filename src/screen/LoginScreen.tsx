12629ac616596aa6a95840335bf7d9b3b9eb45a8
kriptonhaz <kriptonhaz@gmail.com>
2024-06-16 23:21:30 +0700

    implement box transparent

--- src/screen/LoginScreen.tsx
+++ src/screen/LoginScreen.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import {Text, StyleSheet} from 'react-native';
+import {vs} from 'react-native-size-matters';
 import Box from '@components/Box';
+import Theme from '@theme';
 
 export const LoginScreen = () => {
   return (
     <Box.Gradient>
-      <Text>Login Screen</Text>
+      <Box.Transparent>
+        <Text
+          style={[Theme.Typography.TypographyBold.h5, {marginBottom: vs(12)}]}>
+          Login
+        </Text>
+        <Text
+          style={[
+            Theme.Typography.TypographyRegular.sm,
+            {color: Theme.Colors.default.Neutral[30]},
+          ]}>
+          Enter your email and password to log in
+        </Text>
+      </Box.Transparent>
     </Box.Gradient>
   );
 };
