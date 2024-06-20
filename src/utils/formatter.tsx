231f2a176d69c64a8c908ab1f48831e13cfd2e00
kriptonhaz <kriptonhaz@gmail.com>
2024-06-20 11:27:18 +0700

    add utils

--- src/utils/formatter.tsx
+++ src/utils/formatter.tsx
@@ -1,4 +1,5 @@
 import {Dimensions, Platform, PixelRatio} from 'react-native';
+import {moderateScale} from 'react-native-size-matters';
 
 const {width: SCREEN_WIDTH, height: SCREEN_HEIGHT} = Dimensions.get('window');
 
@@ -6,12 +7,13 @@ const {width: SCREEN_WIDTH, height: SCREEN_HEIGHT} = Dimensions.get('window');
 const scale = SCREEN_WIDTH / 320;
 
 const normalize = (size: number) => {
-  const newSize = size * scale;
-  if (Platform.OS === 'ios') {
-    return Math.round(PixelRatio.roundToNearestPixel(newSize));
-  } else {
-    return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2;
-  }
+  return moderateScale(size, 0.3);
+  // const newSize = size * scale;
+  // if (Platform.OS === 'ios') {
+  //   return Math.round(PixelRatio.roundToNearestPixel(newSize));
+  // } else {
+  //   return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2;
+  // }
 };
 
 export {normalize};
