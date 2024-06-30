c5afd8e0d96cb21018f08580add88893d6e8c489
kriptonhaz <kriptonhaz@gmail.com>
2024-06-30 12:34:09 +0700

    add card task and data task

---
+++ src/assets/data/homeTask.data.tsx
@@ -0,0 +1,20 @@
+import {CardTaskProps} from '@components/Card/CardTask';
+
+export const dataTasksHome: CardTaskProps[] = [
+  {
+    id: '1',
+    date: '11/10/2024',
+    time: '16:32',
+    message: 'Tolong di cek ada laporan warga mengenai aktifitas mencurigakan',
+    type: 'Patroli',
+    variant: 'error',
+  },
+  {
+    id: '2',
+    date: '11/10/2024',
+    time: '16:45',
+    message: 'Kepada semua pasukan di harapkan berkumpul di Parkiran Gedung A',
+    type: 'Patroli',
+    variant: 'success',
+  },
+];
---
+++ src/components/Card/CardTask.tsx
@@ -0,0 +1,62 @@
+import {Text, TouchableOpacity, View} from 'react-native';
+import {widthPercentageToDP} from 'react-native-responsive-screen';
+import {mvs} from 'react-native-size-matters';
+import Icon from 'react-native-vector-icons/Feather';
+import {Chip} from '@components/Chip';
+import Theme from '@theme';
+import {heightPercentage, widthPercentage} from '@utils/dimensionFormat';
+
+export type CardTaskProps = {
+  id: string;
+  date?: string;
+  time?: string;
+  message: string;
+  type: string;
+  variant?: 'error' | 'success';
+};
+export const CardTask = (props: CardTaskProps) => {
+  const {date, time, message, type, variant} = props;
+  return (
+    <TouchableOpacity
+      style={{
+        width: widthPercentageToDP('90%'),
+        height: undefined,
+        backgroundColor: Theme.Colors.default.Transparent[1],
+        borderRadius: mvs(16),
+        padding: widthPercentage(20),
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}>
+      <View>
+        <Text
+          style={[
+            Theme.Typography.TypographyItalic.sm,
+            {color: Theme.Colors.default.Neutral[40]},
+          ]}>
+          {date}-{time}
+        </Text>
+        <Text
+          style={[
+            Theme.Typography.TypographyRegular.lg,
+            {color: Theme.Colors.default.Neutral[0]},
+          ]}>
+          {message}
+        </Text>
+        <Chip
+          containerStyle={{
+            backgroundColor: Theme.Colors.default.Error[300],
+            width: widthPercentageToDP('20%'),
+            marginTop: heightPercentage(6),
+          }}>
+          <Text>{type}</Text>
+        </Chip>
+      </View>
+      <Icon
+        name="chevron-right"
+        color={Theme.Colors.default.Neutral[40]}
+        size={widthPercentage(20)}
+      />
+    </TouchableOpacity>
+  );
+};
