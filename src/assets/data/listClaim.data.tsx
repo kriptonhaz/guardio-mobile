b8564d21828485a5518b809ffc71fab2fb2f9f99
kriptonhaz <kriptonhaz@gmail.com>
2024-07-01 08:26:45 +0700

    fix claim screen

---
+++ src/assets/data/listClaim.data.tsx
@@ -0,0 +1,25 @@
+import {ClaimDataType} from '@components/Card/CardClaim';
+
+export const dataListClaim: ClaimDataType[] = [
+  {
+    id: '1',
+    period: 'Fri, 29 Nov - Mon, 2 Dec',
+    remarks: 'Cuti Tahunan',
+    claimPrice: 'Rp. 350.000,-',
+    status: 'Approve',
+  },
+  {
+    id: '2',
+    period: 'Fri, 29 Nov - Mon, 2 Dec',
+    remarks: 'Cuti Melahirkan',
+    claimPrice: 'Rp. 1.500.000,-',
+    status: 'Reviewed',
+  },
+  {
+    id: '3',
+    period: 'Fri, 29 Nov - Mon, 2 Dec',
+    remarks: 'Cuti Nikah',
+    claimPrice: 'Rp. 350.000,-',
+    status: 'Rejected',
+  },
+];
--- src/screen/ClaimScreen.tsx
+++ src/screen/ClaimScreen.tsx
@@ -1,10 +1,73 @@
+import {FlatList, StyleSheet, View} from 'react-native';
+import {widthPercentageToDP} from 'react-native-responsive-screen';
+import {dataListClaim} from '@assets/data/listClaim.data';
 import Box from '@components/Box';
-import {Text} from 'react-native';
+import {CardClaim} from '@components/Card/CardClaim';
+import {Navbar} from '@components/Navbar';
+import {Select} from '@components/Select';
+import {heightPercentage} from '@utils/dimensionFormat';
 
 export const ClaimScreen = () => {
   return (
-    <Box.Gradient>
-      <Text>Claim Screen</Text>
+    <Box.Gradient containerStyle={styles.root}>
+      <Navbar label="Claim" rightIcon="plus-circle" />
+      <View style={styles.wrapperSelect}>
+        <Select
+          containerStyle={styles.selectDate}
+          type="date"
+          value="29/09/2024"
+        />
+        <Select
+          containerStyle={styles.selectType}
+          type="options"
+          value="Type"
+        />
+      </View>
+      <FlatList
+        data={dataListClaim}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={item => item.id}
+        style={{marginTop: heightPercentage(27)}}
+        ItemSeparatorComponent={() => {
+          return (
+            <View
+              style={{
+                width: '100%',
+                marginTop: heightPercentage(12),
+              }}
+            />
+          );
+        }}
+        renderItem={({item}) => (
+          <CardClaim
+            id={item.id}
+            period={item.period}
+            remarks={item.remarks}
+            claimPrice={item.claimPrice}
+            status={item.status}
+          />
+        )}
+      />
     </Box.Gradient>
   );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    justifyContent: 'flex-start',
+    paddingTop: heightPercentage(70),
+  },
+  wrapperSelect: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: widthPercentageToDP('90%'),
+    marginTop: heightPercentage(28),
+  },
+  selectDate: {
+    width: widthPercentageToDP('50%'),
+  },
+  selectType: {
+    width: widthPercentageToDP('30%'),
+  },
+});
